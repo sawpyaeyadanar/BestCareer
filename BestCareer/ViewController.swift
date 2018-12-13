b6f47b624d24a6c2ff64345b95dcef0c12d94e97
Saw Pyae Yadanar <84pinklady@gmail.com>
2018-12-13 16:44:09 +0630

    Initial Commit

---
+++ BestCareer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestCareer
+//
+//  Created by Saw Pyae Yadanar on 13/12/18.
+//  Copyright © 2018 Saw Pyae Yadanar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
